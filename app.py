bbb5726e050aefaf412d310378e6cb7b9e8c370b
IlyaDeskov <Ilya.Deskov@yandex.ru>
2017-10-05 11:32:54 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -72,9 +72,9 @@ def createApp():
                     Synchronizedcards = list(filter(lambda a: config.SYNC_LABEL_NAME in [l['name'] for l in a['labels']] and a['name'] == updatedcardname,json.loads(boardcards.text)))
                     if Synchronizedcards:
                         for crdid in [c['id'] for c in Synchronizedcards]:
-                        app.logger.info('Synchronizind with card '+ crdid)
-                        resu = requests.put('https://api.trello.com/1/cards/'+ crdid+'?name=Renamed%20by%20api' + config.CREDENTIALS_STR)
-                        app.logger.info(resu.text)
+                            app.logger.info('Synchronizind with card '+ crdid)
+                            resu = requests.put('https://api.trello.com/1/cards/'+ crdid+'?name='+updatedcardname + config.CREDENTIALS_STR)
+                            app.logger.info(resu.text)
         # Set the next thread to happen
         queueWorker = threading.Timer(config.CHECK_TIME, doStuff, ())
         queueWorker.start()   
